3b4e7fcc69b3949446263822d91d14a465551668
billykagwe <billmwas2@gmail.com>
2023-02-23 13:10:49 +0300

    styling login page and adding env variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,13 @@ import Chat from "./components/Chat/Chat";
 import Sign from "./components/Auth/signin";
 import { FronteggProvider } from "@frontegg/react";
 
-const contextOptions = {
-  baseUrl: "https://app-5omj9h9oozk4.frontegg.com",
-  clientId: "b85b111c-47f0-4ce8-bcc9-e2fed2085d8e",
-};
+console.log({ process: process.env });
 
 function App() {
+  const contextOptions = {
+    baseUrl: process.env.REACT_APP_FRONTEGG_BASE_URL,
+    clientId: process.env.REACT_APP_FRONTEGG_CLIENT_ID,
+  };
   return (
     <BrowserRouter>
       <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,21 +5,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { FronteggProvider } from "@frontegg/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const contextOptions = {
-  baseUrl: "https://app-5omj9h9oozk4.frontegg.com",
-  clientId: "b85b111c-47f0-4ce8-bcc9-e2fed2085d8e",
-};
-
 root.render(
   <React.StrictMode>
-    {/* <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}> */}
     <App />
-    {/* </FronteggProvider> */}
   </React.StrictMode>
 );
 
